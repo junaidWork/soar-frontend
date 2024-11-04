906b46a0d026828bca4d3df4de38a89b067d156c
junaid-islam <junaidkhan6212@gmail.com>
2024-11-04 23:51:51 +0500

    design-layout-and-sidebar
    - designed the sidebae
    - designed the header
    - designed the application layout

---
+++ src/utils/contants.ts
@@ -0,0 +1,21 @@
+import Home from '../assets/icons/home.svg';
+import Transactions from '../assets/icons/Glyph.svg';
+import User from '../assets/icons/user.svg';
+import Investments from '../assets/icons/investment.svg';
+import CreditCards from '../assets/icons/credit-card.svg';
+import Loans from '../assets/icons/loan.svg';
+import Services from '../assets/icons/service.svg';
+import Setting from '../assets/icons/settings.svg';
+import Privileges from '../assets/icons/privilege.svg';
+
+export const sideBarMenuItems = [
+  { icon: Home, text: 'Dashboard', active: true },
+  { icon: Transactions, text: 'Transactions' },
+  { icon: User, text: 'Accounts' },
+  { icon: Investments, text: 'Investments' },
+  { icon: CreditCards, text: 'Credit Cards' },
+  { icon: Loans, text: 'Loans' },
+  { icon: Services, text: 'Services' },
+  { icon: Privileges, text: 'My Privileges' },
+  { icon: Setting, text: 'Setting' }
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#343C6A',
+        secondary: '#B1B1B1'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
